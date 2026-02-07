ccf1f8d262f73980b84cc3733d547992fd027346
Meus Apps <lgnappswebs@gmail.com>
2026-02-07 22:53:48 +0000

    ## Error Type Build Error
    ## Error Message
    Parsing ecmascript source co

--- src/app/admin/banners/page.tsx
+++ src/app/admin/banners/page.tsx
@@ -337,7 +337,7 @@ export default function AdminBannersPage() {
                 </Link>
               </DropdownMenuItem>
               <DropdownMenuItem asChild>
-                <Link href="/admin/banners" className="flex items-center h-10 rounded-xl text-primary font-bold">
+                <Link href="/admin/banners" className="flex items-center h-10 rounded-xl text-black">
                   <ImageIcon className="mr-2 h-4 w-4 text-orange-500" /> Banners
                 </Link>
               </DropdownMenuItem>
